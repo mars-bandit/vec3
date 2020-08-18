26984bb1c488ec509b1c8dcd91e10c5a626b53a3
blfunex <blfunex@gmail.com>
2020-08-18 15:49:29 +0100

    Added cross, clamp, angle and linear, some iteration functions

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 _TypeScript 3D Vector_ library based on iterators and arrays, **UNCOMPILED**, _use `"moduleResolution": "node"`_.
 
 Inspired by glMatrix and glm.
+
+Like glMatrix we will flatten iteration, but unlike glMatrix, we will not inline function calls, we will let that task to the minifier and / or JIT.
--- index.ts
+++ index.ts
@@ -11,6 +11,12 @@ namespace vec3 {
     return [x, y, z];
   }
 
+  export function from(value: vec3.readonly): vec3;
+
+  export function from([x, y, z]: vec3.readonly): vec3 {
+    return vec3.of(x, y, z);
+  }
+
   export function set(out: vec3, x: number, y: number, z: number) {
     out[0] = x;
     out[1] = y;
@@ -19,11 +25,13 @@ namespace vec3 {
   }
 
   export function copy(out: vec3, source: vec3.readonly): vec3;
+
   export function copy(out: vec3, [x, y, z]: vec3.readonly) {
     return vec3.set(out, x, y, z);
   }
 
   export function add(out: vec3, a: vec3.readonly, b: vec3.readonly): vec3;
+
   export function add(
     out: vec3,
     [ax, ay, az]: vec3.readonly,
@@ -36,6 +44,7 @@ namespace vec3 {
   }
 
   export function sub(out: vec3, a: vec3.readonly, b: vec3.readonly): vec3;
+
   export function sub(
     out: vec3,
     [ax, ay, az]: vec3.readonly,
@@ -48,6 +57,7 @@ namespace vec3 {
   }
 
   export function mul(out: vec3, a: vec3.readonly, b: vec3.readonly): vec3;
+
   export function mul(
     out: vec3,
     [ax, ay, az]: vec3.readonly,
@@ -60,6 +70,7 @@ namespace vec3 {
   }
 
   export function div(out: vec3, a: vec3.readonly, b: vec3.readonly): vec3;
+
   export function div(
     out: vec3,
     [ax, ay, az]: vec3.readonly,
@@ -72,6 +83,7 @@ namespace vec3 {
   }
 
   export function dot(a: vec3.readonly, b: vec3.readonly): number;
+
   export function dot(
     [ax, ay, az]: vec3.readonly,
     [bx, by, bz]: vec3.readonly
@@ -80,11 +92,13 @@ namespace vec3 {
   }
 
   export function length(vector: vec3.readonly): number;
+
   export function length([x, y, z]: vec3.readonly) {
     return Math.hypot(x, y, z);
   }
 
   export function lengthSq(vector: vec3.readonly): number;
+
   export function lengthSq([x, y, z]: vec3.readonly) {
     return x ** 2 + y ** 2 + z ** 2;
   }
@@ -94,6 +108,7 @@ namespace vec3 {
     vector: vec3.readonly,
     scale: number
   ): vec3;
+
   export function scale(
     out: vec3,
     [x, y, z]: vec3.readonly,
@@ -110,7 +125,118 @@ namespace vec3 {
     return scale(out, vector, r >= 0 ? 1 / Math.sqrt(r) : 0);
   }
 
+  export function cross(
+    out: vec3,
+    a: vec3.readonly,
+    b: vec3.readonly
+  ): vec3;
+
+  export function cross(
+    out: vec3,
+    [ax, ay, az]: vec3.readonly,
+    [bx, by, bz]: vec3.readonly
+  ) {
+    out[0] = ay * bz - by * az;
+    out[1] = az * bx - bz * ax;
+    out[2] = ax * by - bx * ay;
+    return out;
+  }
+
+  function _clamp(x: number, min: number, max: number) {
+    return Math.min(max, Math.max(min, x));
+  }
+
+  export function clamp(
+    out: vec3,
+    [x, y, z]: vec3.readonly,
+    min: number,
+    max: number
+  ) {
+    out[0] = _clamp(y, min, max);
+    out[1] = _clamp(z, min, max);
+    out[2] = _clamp(x, min, max);
+    return out;
+  }
+
+  // https://www.analyzemath.com/stepbystep_mathworksheets/3D_vectors/angle_vectors.png?ezimgfmt=rs:338x165/rscb1/ngcb1/notWebP
+
+  export function angle(a: vec3.readonly, b: vec3.readonly) {
+    const mag = length(a) * length(b);
+    const cosine = mag && dot(a, b) / mag;
+    return Math.acos(_clamp(cosine, -1, 1));
+  }
+
+  export function linear(
+    out: vec3,
+    a: vec3.readonly,
+    b: vec3.readonly,
+    t: number
+  ): vec3;
+
+  export function linear(
+    out: vec3,
+    [ax, ay, az]: vec3.readonly,
+    [bx, by, bz]: vec3.readonly,
+    t: number
+  ) {
+    out[0] = ax + (bx - ax) * t;
+    out[1] = ay + (by - ay) * t;
+    out[2] = az + (bz - az) * t;
+    return out;
+  }
+
+  // TODO: export function quadratic(out: vec3, a: vec3.readonly, c: vec3.readonly, d: vec3.readonly, t: number): vec3;
+  // TODO: export function cubic( out: vec3, a: vec3.readonly, b: vec3.readonly, c: vec3.readonly, d: vec3.readonly, t: number): vec3
+
+  const EAX = vec3.create();
+  const EBX = vec3.create();
+
+  export function each(
+    source: number[],
+    visit: (
+      vector: vec3.readonly,
+      source: number[],
+      index: number
+    ) => void,
+    stride = 3,
+    offset = 0
+  ) {
+    for (let i = offset; i < source.length; i += stride) {
+      visit(
+        vec3.set(EAX, source[i], source[i + 1], source[i + 2]),
+        source,
+        i
+      );
+    }
+  }
+
+  export function map(
+    source: number[],
+    map: (
+      vector: vec3.readonly,
+      out: vec3,
+      source: number[],
+      index: number
+    ) => vec3,
+    stride = 3,
+    offset = 0,
+    clone = true
+  ): Iterable<number> {
+    if (stride < 3) throw "vec3.map's stride can not be lower than 3.";
+    if (offset < 0) throw "vec3.map's offset can not be lower than 0.";
+    const result = clone ? source.slice(0) : source;
+    for (let i = offset; i < source.length; i += stride) {
+      vec3.set(EAX, source[i], source[i + 1], source[i + 2]);
+      const [x, y, z] = map(EAX, EBX, source, i);
+      result[i] = x;
+      result[i + 1] = y;
+      result[i + 2] = z;
+    }
+    return result;
+  }
+
   export function string(vector: vec3.readonly): string;
+
   export function string([x, y, z]: vec3.readonly) {
     return `(${x.toFixed(2)}, ${y.toFixed(2)}, ${z.toFixed(2)})`;
   }
